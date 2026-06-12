731ddad411c6acba9e2a0e707117dcc9fece461f
locoviera24-code <292920158+locoviera24-code@users.noreply.github.com>
2026-06-11 21:21:54 -0300

    Alinea fecha e hidratacion del asistente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protocolo-0-100-pwa-v4';
+const CACHE_NAME = 'protocolo-0-100-pwa-v5';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting()));
