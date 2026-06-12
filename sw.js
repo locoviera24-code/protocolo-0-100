c1a1a1e45ea75e7374a81b31e20b55e67460a3e5
locoviera24-code <292920158+locoviera24-code@users.noreply.github.com>
2026-06-11 21:24:16 -0300

    Pulir vista previa nutricional

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protocolo-0-100-pwa-v5';
+const CACHE_NAME = 'protocolo-0-100-pwa-v6';
 const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting()));
